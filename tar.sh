89763b186f635c10eb2645da04ff48adb724f382
Adrián Tóth <adricko28@gmail.com>
2018-11-22 10:59:46 +0100

    fix: pipe + some notes

--- tar.sh
+++ tar.sh
@@ -2,8 +2,8 @@
 
 LOGIN=xtotha01
 
-rm -f ${LOGIN}.tar.gz
+rm -fv ${LOGIN}.tar.gz
 
 cp doc/doc.pdf src/secrets.txt .
 tar -zcvf ${LOGIN}.tar.gz doc.pdf secrets.txt
-rm doc.pdf secrets.txt
+rm -f doc.pdf secrets.txt
